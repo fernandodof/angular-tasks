d3aa1b674c564ec9eea89930ca6ab9f5a87ffc09
Fernando <fernandodof@gmail.com>
2015-09-22 23:26:29 -0300

    Finishig initial setup

--- public_html/js/angular-modules/app.js
+++ public_html/js/angular-modules/app.js
@@ -1,33 +1,58 @@
 var app = angular.module('tasks', [
-    'jcs-autoValidate'
+    'jcs-autoValidate',
+    'angular-ladda'
 ]);
 
-app.run(function (defaultErrorMessageResolver){
-    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages){
-       errorMessages['shotTitle'] = 'You must type at least {} characters'; 
-       errorMessages['longTitle'] = 'You must type no more than {} characters'; 
-       errorMessages['badTitle'] = 'You must type only numbers, letters, "_" and "-"'; 
+app.run(function (defaultErrorMessageResolver) {
+    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+        errorMessages['shotTitle'] = 'You must type at least {} characters';
+        errorMessages['longTitle'] = 'You must type no more than {} characters';
+        errorMessages['badTitle'] = 'You must type only numbers, letters, "_" and "-"';
     });
 
 });
 
-var api_base = "http://localhost:8000/api";
-app.controller('TaskControler', function ($scope, $http){
-    $scope.formModel = {};   
-    
-    $scope.onSubmit = function (){
-        console.log("submited");
+var api_base = 'http://192.186.0.102:8000/api/';
+
+app.controller('TaskControler', function ($scope, $http) {
+    $scope.formModel = {};
+    $scope.submiting = false;
+
+    $scope.onSubmit = function () {
+        $scope.submiting = true;
+        console.log('submited');
         console.log($scope.formModel);
-        
-        $http.post(api_base+"/tasks/", $scope.formModel)
-            .success(function (data){
-               console.log(data);
-               console.log("\\o/");
-            })
-            .error(function (data){
-               console.log(data);
-               console.log(":-)");
-            });
+
+        $http.post(api_base + '/tasks/', $scope.formModel)
+                .success(function (data) {
+                    console.log(data);
+                    console.log('\\o/');
+                    $scope.submiting = false;
+                    $scope.tasks.push(data);
+                })
+                .error(function (data) {
+                    console.log(data);
+                    console.log(':-)');
+                    $scope.submiting = false;
+                });
     };
-    
+
+    $scope.selectedTaskIndex = null;
+    $scope.selectedTask = null;
+    $scope.selectTask = function (task, index) {
+        $scope.selectedTaskIndex = index;
+        $scope.selectedTask = task;
+    };
+
+    $scope.tasks = {};
+    $http.get(api_base + '/tasks/')
+            .success(function (data) {
+                $scope.tasks = data;
+            })
+            .error(function (data) {
+                console.log(data);
+            });
+
+
+
 });
