e3d1d285acfeafde04609bb940e47ecf3a123141
fernandodof <fernandodof@gmail.com>
2015-09-30 16:07:41 -0300

    using pagination

--- public_html/js/angular-modules/app.js
+++ public_html/js/angular-modules/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('phonebook', [
     'jcs-autoValidate',
-    'angular-ladda'
+    'angular-ladda',
+    'ngResource',
+    'infinite-scroll'
 ]);
 
 app.run(function (defaultErrorMessageResolver) {
@@ -16,17 +18,31 @@ app.run(function (defaultErrorMessageResolver) {
 
 var api_base = 'http://localhost:8000/api';
 
-app.controller('PersonControler', function ($scope, $http) {
+
+app.config(function ($httpProvider, $resourceProvider) {
+    $resourceProvider.defaults.stripTralingSlashes = false;
+});
+
+app.factory('People', function ($resource) {
+    return $resource(api_base + '/person-paginated/', {}, {
+        query: {method: 'GET', isArray: false}
+    });
+});
+
+app.controller('PersonDetailController', function ($scope, PersonService) {
+    $scope.personServiceRef = PersonService;
+});
+
+app.controller('PersonListController', function ($scope, $http, PersonService) {
     $scope.formModel = {};
 //
     $scope.formModel.photo = "http://lauriefurman.com/facebook-profile-picture-silhouette-funny-713.jpg";
 //
     $scope.submiting = false;
     $scope.search = '';
-    $scope.selectedPerson = null;
-    $scope.people = {};
     $scope.order = "email";
-    
+    $scope.personServiceRef = PersonService;
+    $scope.people = [];
     //Submit form
     $scope.onSubmit = function () {
         $scope.submiting = true;
@@ -47,24 +63,6 @@ app.controller('PersonControler', function ($scope, $http) {
                 });
     };
 
-    //Select Person
-
-
-    $scope.selectPerson = function (person) {
-        $scope.selectedPerson = person;
-    };
-
-    // Get list
-    $http.get(api_base + '/person/')
-            .success(function (data) {
-                $scope.people = data;
-            })
-            .error(function (data) {
-                console.log(data);
-            });
-
-
-
     $scope.sensitiveSearch = function (person) {
         if ($scope.search) {
             return person.name.toLowerCase().indexOf($scope.search.toLowerCase()) === 0 ||
@@ -73,5 +71,72 @@ app.controller('PersonControler', function ($scope, $http) {
         }
         return true;
     };
+
+    $scope.loadMorePeople = function () {
+        console.log('Load More');
+        $scope.personServiceRef.loadMore();
+    };
+});
+
+app.service('PersonService', function ($http, People) {
+
+    var peopleList = [];
+
+    var self = {
+        'addPerson': function (person) {
+            $http.post(api_base + '/person/', person)
+                    .success(function (data) {
+                        console.log(data);
+                        console.log('\\o/');
+                        $scope.submiting = false;
+                        $scope.people.push(data);
+                    })
+                    .error(function (data) {
+                        console.log(data);
+                        console.log(':-)');
+                        $scope.submiting = false;
+                    });
+        },
+        'page' : 1,
+        'hasMore' : true,
+        'isLoading' : false,
+        'selectedPerson': null,
+        'peopleList': [],
+        'loadPeople': function () {
+            if(self.hasMore && !self.isLoading){
+                self.isLoading = true;
+                
+                var params = {
+                    'page' : self.page
+                };
+                
+                People.query(params , function (data) {
+                    angular.forEach(data.results, function(person){
+                        self.peopleList.push(new People(person));
+                    });
+//                    self.peopleList.push(data.results);
+                    console.log(data);
+
+                    if(!data.next){
+                        self.hasMore = false;
+                    }
+                    self.isLoading = false;
+                });
+            }
+            
+        },
+        'loadMore': function (){
+            if(self.hasMore && !self.isLoading){
+                self.page += 1;
+                self.loadPeople();
+            }
+        }
+
+
+    };
+
+    self.loadPeople();
+
+    return self;
 
 });
